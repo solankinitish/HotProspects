8a46d84afac312e1264354a74b1ca6d16696b536
solankinitish <nitishsolanki888@gmail.com>
2022-04-25 06:41:41 +0530

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Nitish Solanki on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
